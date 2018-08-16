a6136801a87b05108f55c6d482744644953e936e
lolo <luo2luo2luo2@gmail.com>
2018-08-16 09:04:11 +0800

    提交次数：第2次

--- classbest.py
+++ classbest.py
@@ -4,36 +4,48 @@
 @author: Jack Luo
 @file: classbest.py
 '''
-import requests
 import pandas as pd
+import requests
 
 
 class Best(object):
-    def __init__(self, hangye, nums=30):
+    def __init__(self, hangye, daima, nums=30):
         self.hangye = hangye
+        self.daima = daima
         self.nums = nums
+        self.headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'}
+        self.url = f'https://best.zhaopin.com/api/zhaopin/zpcompanylabsum/page?insId={self.daima}&pageNumber={self.nums}&currentPage=1&type=0&at=7d409fdabf954d23af9db0ca0263db3d&rt=540cff9e9fa74cdeaa3880b42322a86b'
 
     def get_infos(self):
         paiming = []
         votes = []
         name = []
-        url = f'https://best.zhaopin.com/api/zhaopin/zpcompanylabsum/page?insId={self.hangye}&pageNumber={self.nums}&currentPage=1&type=0&at=7d409fdabf954d23af9db0ca0263db3d&rt=540cff9e9fa74cdeaa3880b42322a86b'
-        resp = requests.get(url).json()
+        resp = requests.get(self.url, headers=self.headers)
+        resp = resp.json()
+
         for i in resp['data']['pp']:
             paiming.append(i['ranking'])
             votes.append(i['labPointCount'])
             name.append(i['companyInfo']['coName'])
-        return {'排名': paiming, '数据': {'支持': votes, '名称': name}}
+        return {'排名': paiming, '数据': {'支持票': votes, f'{self.hangye}': name}}
 
-    def df(self):
-        get=self.get_infos()
+    def get_dataframe(self):
+        get = self.get_infos()
         datas = pd.DataFrame(get['数据'], index=get['排名'])
         datas.index.name = '排名'
         return datas
 
-    def pr(self):
-        datas=self.df()
-        print(datas)
+    def write_excel(self):
+        datas = self.get_dataframe()
+        datas.to_excel(w, sheet_name=f'{self.hangye}')
 
-jinrong=Best('11100')
-jinrong.pr()
+
+if __name__ == '__main__':
+    hangyes = ['政府', '农林', '金融']
+    daimas = [11100, 11400, 10200]
+    with pd.ExcelWriter('votes.xls') as w:
+        for h, d in zip(hangyes, daimas):
+            datas = Best(h, d)
+            datas.write_excel()
+        w.save()
